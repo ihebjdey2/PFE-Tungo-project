546ddaa332e2b4703f8de3c8a21038de7a9046c6
Iheb Jdey <jdey.iheb@esprit.tn>
2025-07-16 02:56:18 +0100

    commit n'4 ll

--- android/app/src/main/kotlin/com/example/reservation_louage_frontend/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.reservation_louage_frontend
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/utils/constants.dart
+++
@@ -1,3 +0,0 @@
-class ApiConstants {
-  static const String baseUrl = 'http://10.0.2.2:3000/api'; // Émulateur Android
-}
