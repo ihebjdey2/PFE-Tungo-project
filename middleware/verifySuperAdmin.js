d45a1305dca108de2c39bc8d83a9dcc52383a6fe
ihebjdey2 <jdey.iheb@esprit.tn>
2026-06-18 19:49:51 +0100

    reset main branch

--- middleware/verifySuperAdmin.js
+++
@@ -1,7 +0,0 @@
-module.exports = (req, res, next) => {
-    if (!req.user || req.user.role !== 'SuperAdmin' || req.user.id !== 1) {
-      return res.status(403).json({ message: "Accès strictement réservé au SuperAdmin principal." });
-    }
-    next();
-  };
-  
--- routes/reset_password.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController');
-
-router.post('/request-reset', authController.requestPasswordReset);
-router.post('/reset-password', authController.resetPassword);
-
-module.exports = router;
